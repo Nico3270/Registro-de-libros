6a9d6e30c4a36b188e4e8e8c62b5fe65520331c6
Nicolás R <cristianrodriguez3270@hotmail.com>
2022-09-21 18:09:49 -0500

    Conectando con base de datos SQMLAlme

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 app = Flask(__name__)
-
 all_books = []
 
+
+#Creando una base de datos con SQLALchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float(250), nullable=False)
+
+db.create_all()
+new_book = Book(id=2, title="El Sindrome de Ulises", author="J. K. Rowling", rating=9.5)
+db.session.add(new_book)
+db.session.commit()
 
 @app.route('/')
 def home():
