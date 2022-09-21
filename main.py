58e4554de136328bc6c028e3d9d4f948d78b8856
Nicolás R <cristianrodriguez3270@hotmail.com>
2022-09-21 15:13:17 -0500

    libretia funcionando sin base de datos

--- main.py
+++ main.py
@@ -7,12 +7,21 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    return render_template('index.html', libros = all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["POST", "GET"])
 def add():
-    pass
+    if request.method == "POST":
+        book_data = {}
+        data = request.form
+        book_data["title"] = data["title"]
+        book_data["author"] = data["author"]
+        book_data["rating"] = data["rating"]
+        all_books.append(book_data)
+        print(all_books)
+        return redirect(url_for('home'))
+    return render_template('add.html')
 
 
 if __name__ == "__main__":
